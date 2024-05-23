bb5e700c1409426c75d9a1392202bb42a020d111
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-05-24 04:02:17 +0800

    Update setup.sh

--- rocket.chat/setup.sh
+++ rocket.chat/setup.sh
@@ -1,3 +1,3 @@
-docker run --name db -d mongo:4.0 --smallfiles --replSet rs0 --oplogSize 128
+docker run --name db -d mongo:4.4 --replSet rs0 --oplogSize 128
 docker exec -ti db mongo --eval "printjson(rs.initiate())"
 docker run --name rocketchat -p 7005:3000 --link db --env ROOT_URL=http://localhost:7005 --env MONGO_OPLOG_URL=mongodb://db:27017/local -d rocket.chat
